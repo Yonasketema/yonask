e630a527cd0acc8557085fb59ac027ded78a3c3f
yonasketema <yonask.kitaw@gmail.com>
2023-07-26 12:56:42 +0300

    add icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,54 @@
+import { FiGithub } from "react-icons/fi";
+import { BiLogoLinkedin } from "react-icons/bi";
+
 const Footer = () => {
   return (
-    <p style={{ margin: "3rem 0", textAlign: "center" }}>
+  <>
+    <p style={{ margin: "1rem 0", textAlign: "center" }}>
       design &amp; develop by yonas ketema
+      
+      
+         
     </p>
-  );
+    
+    <div    
+        className="footer__link"
+        style={{
+          justifyContent: "center",
+          alignContent: "center",
+          gap: "1.2rem",
+          
+           margin: "1rem 0"
+        }}>
+    <button
+          style={{
+            color: "#fff",
+            backgroundColor: "transparent",
+            border: "none",
+            width: "1.5rem",
+            fontSize: "1.5rem",
+          }}
+        >
+          <a href="https://www.linkedin.com/in/yonaskk">
+            <BiLogoLinkedin />
+          </a>
+        </button>
+        <button
+          style={{
+            color: "#fff",
+            backgroundColor: "transparent",
+            border: "none",
+            width: "1.5rem",
+            fontSize: "1.5rem",
+          }}
+        >
+          <a href="https://github.com/Yonasketema">
+            <FiGithub />
+          </a>
+        </button>
+    </div>
+    </>
+    );
 };
 
 export default Footer;
